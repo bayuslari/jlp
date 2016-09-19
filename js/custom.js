0f42dc8e35c0519a4be2b828545e49563a214fbe
Bayu Slamet Riyadi <bayuslari@Bayus-MacBook-Pro.local>
2016-09-19 15:55:32 +0700

    fix timeline, add text-form, and user list

--- js/custom.js
+++ js/custom.js
@@ -2,20 +2,47 @@
 var app = {
     init: function() {
         app.navbarChange();
+        app.submenuNormalize();
+        app.textForm();
+        app.datePick();
     },
-    resize: function(){
+    resize: function() {
 
     },
     navbarChange: function() {
         jQuery('#menu-toggle').click(function() {
-        	jQuery('body').toggleClass('sm-nav');
+            jQuery('body').toggleClass('sm-nav');
+        });
+    },
+    submenuNormalize: function() {
+        jQuery(document).click(function(event) {
+            if (!jQuery(event.target).closest('.side-nav li').length) {
+                jQuery(".sm-nav .side-nav li , .side-nav li a").removeClass('active');
+                jQuery(".sm-nav .collapsible-body").css("display", "none")
+            }
+        })
+    },
+    textForm: function() {
+        var simplemde = new SimpleMDE({ element: jQuery("#text-form")[0] });
+    },
+    datePick: function() {
+        jQuery('.datepicker').pickadate({
+            selectMonths: true,
+            selectYears: 40,
+            min: new Date(1990, 0, 1),
+            max: new Date(2000, 11, 31),
+            closeOnSelect: true,
+        });
+        jQuery("#dob").click(function(event) {
+            event.stopPropagation();
+            jQuery(".datepicker").first().pickadate("picker").open();
         });
     },
 };
 
 jQuery(document).ready(function($) {
     app.init();
-    jQuery(window).on("resize", function() {
+    $(window).on("resize", function() {
         app.resize();
     });
 });
